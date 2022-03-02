9c65323824f9208ff14913cf41938c888ec161da
Rodrigo Orozco Hidalgo <47331828+zikln@users.noreply.github.com>
2022-03-02 13:06:02 -0700

    Update app_logic.py

--- app_logic.py
+++ app_logic.py
@@ -1,72 +1,125 @@
 """
 All functions and logic of the app must be defined here
 """
-content_generator(string):
+TRUTH_TABLE_VARS = {}
+KEYS=[]
+ORIGINAL_EXPRESSION = None
+def content_generator(string):
+	global ORIGINAL_EXPRESSION 
+	ORIGINAL_EXPRESSION = string
 	content = []
+	global TRUTH_TABLE_VARS
+	global KEYS
 	for i in range(2,len(string)):
-		if(string[i-2]="-" and string[i-1]=">"):
-			content.append("i");
-		elif(string[i-2]="<" and string[i-1]="-" and string[i] == ">"):
+		if(string[i-2]=="-" and string[i-1]==">"):
+			if(content[-1]!="d"):
+				content.append("i");
+			else:
+				continue
+		elif(string[i-2]=="<" and string[i-1]=="-" and string[i] == ">"):
 			content.append("d");
 		else:
 			content.append(string[i-2])
-	content.append(string[-2])
-	content.append(string[-1])
+	content.append(string[len(string)-2])
+	content.append(string[len(string)-1])
+	while(">"in content):
+		content.remove(">")
+	for ltr in content:
+		if ltr in['p','q','r','s','t']:
+			TRUTH_TABLE_VARS[ltr] = False
+	KEYS = list(TRUTH_TABLE_VARS.keys())
 	return content
 
-content_reformatter(content):
-	for i in range(len(string)):
+def content_reformatter(content):
+	for i in range(len(content)):
 		if(content[i] == "^"):
-			content[i] = " or "
-		elif(content[i] == "v"):
 			content[i] = " and "
+		elif(content[i] == "v"):
+			content[i] = " or "
 		elif(content[i] == "~"):
 			content[i] = " not "
 		else:
 			pass
-	for i in range(len(string)):
-		if(content[i] == "i"):
-			content [0] = " not ( "+content[0]
-			content [i-1] +=  " ) "
-			content [i] = " or "
+	early_outputs = []
+	out = []
+	title = []
+	prev = 0
+	for i in range(len(content)):
+		if(content[i] == "i"):	
+			title.append("".join(content[prev:i]))
+			out.append(evaluator("".join(content[prev:i])))
+			prev = i+1 	
+			content[0] = " not(  "+content[0]
+			content[i-1] +=  " ) "
+			content[i] = " or "
+			
 		elif(content[i] == "d"):
-			content[i-1], content[i], content[i+1] = " ( "+content[i-1]+ " and " + content [i+1]+" ) ", " or ", ( not "+content[i-1]+ " and not "  + content [i+1]+" ) "			
+			title.append("".join(content[prev:i]))
+			out.append(evaluator("".join(content[prev:i])))
+			prev = i+1
+			content[i-1], content[i], content[i+1] = " ( "+content[i-1]+ " and " + content [i+1]+" ) ", " or "," ( not "+content[i-1]+ " and not "  + content [i+1]+" )"			
 		else: pass
-		return "".join(content)
+	title.append("".join(content[prev:len(content)]))
+	out.append(evaluator("".join(content[prev:len(content)])))
+	final_expression = "".join(content)
+	title.append(final_expression)
+	out.append(evaluator(final_expression))
+	return title, out
 
-truth_table_generator(input):
-	p = 0
-	q = 0
-	r = 0
-	s = 0
-	t = 0
-	
+
+
+def evaluator(inputs):
+	global TRUTH_TABLE_VARS
+	global KEYS
 	results = []
-	for i in range(32):
-		results.append(eval(input))
-		if (t == 0):
-			t++
-		else:
-			t--
-		if (i%2):
-			if (s == 0):
-				s++
-			else:
-				s--
-			if(i%4):
-				if (r == 0):
-					r++
-				else:
-					r--
-				if(i%8):
-					if (q == 0):
-						q++
-					else:
-						q--
-					if(i%16):
-						if(p == 0):
-							p++
-						else:
-							p--
+	for i in range(1,1+2**len(KEYS)):
+		results.append(eval(inputs,TRUTH_TABLE_VARS))
+		divisor = 1
+		index = len(KEYS) 
+		while(i%divisor == 0):
+			divisor *=2
+			index -= 1
+			key = KEYS[index]
+			TRUTH_TABLE_VARS[key] = not TRUTH_TABLE_VARS[key]
+		
 	return results
 	
+def variables_columns_generator():
+	global TRUTH_TABLE_VARS
+	global KEYS
+	values = []
+	for i in range(1,1+2**len(KEYS)):
+		values.append([])
+		for k in KEYS:
+			values[-1].append(TRUTH_TABLE_VARS[k])
+		divisor = 1
+		index = len(KEYS) 
+		while(i%divisor == 0):
+			divisor *=2
+			index -= 1
+			key = KEYS[index]
+			TRUTH_TABLE_VARS[key] = not TRUTH_TABLE_VARS[key]
+	return values
+
+def build_Table( Main_columns, inputs):
+	table = KEYS[:]
+	titles, results = inputs
+	titles[-1] = ORIGINAL_EXPRESSION
+	table += titles
+	transposed_results = [[results[j][i] for j in range(len(results))] for i in range(len(results[0]))]
+	for i in range(len(Main_columns)):
+		Main_columns[i] += transposed_results[i]
+	table = [table]	
+	table+= Main_columns
+	
+	return table
+
+
+if __name__ == "__main__":
+	string = input()
+	content = content_generator(string)
+	inputs = content_reformatter(content)
+	Main_columns = variables_columns_generator()
+	table = build_Table(Main_columns,inputs) 
+	for row in table:
+		print(row)
